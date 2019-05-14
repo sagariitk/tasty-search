56c7720564a7865d4aa20153edd80149d49954cd
Sagar Yadav <sagaryadaviitk@gmail.com>
2019-05-14 22:31:40 +0530

    ds

--- edge_react/src/components/Landing.js
+++ edge_react/src/components/Landing.js
@@ -12,7 +12,7 @@ class Landing extends Component {
             const body = {
                 "search_string": search_string
             }
-            const api_call = await fetch(`http://3.15.31.2:80/search`, {
+            const api_call = await fetch(`http://localhost:80/search`, {
                 method: 'POST',
                 body: JSON.stringify(body),
                 headers: {
